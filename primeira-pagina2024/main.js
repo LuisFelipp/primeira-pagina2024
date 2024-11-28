1391a9bde039250a43d79d6a5994a258e1400d2e
Luis Felipp <luisziba@hotmail.com>
2024-11-27 22:22:47 -0300

    adiciona imagem

--- primeira-pagina2024/main.js
+++ primeira-pagina2024/main.js
@@ -5,19 +5,8 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <h1> Versionando minha aplicação web<h1>
+    <img src="https://archives.sva.edu/uploads/post/486c07fc9955b994aa92080dec4ee2f6.jpg" >
   </div>
 `
 
